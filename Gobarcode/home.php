08aeaa03037794cf50d5df41e92de888e7917123
Johnny-TW <a0988298334@gmail.com>
2020-11-15 21:24:36 +0800

    Gobar start
    起始檔案歸類

---
+++ Gobarcode/home.php
@@ -0,0 +1,10 @@
+<?php include __DIR__ . '/parts/config.php'; ?>
+<?php include __DIR__ . '/parts/html-header.php'; ?>
+<?php include __DIR__ . '/parts/navbar.php'; ?>
+
+<div class="container">
+    <h2>GoBar</h2>
+</div>
+
+<?php include __DIR__ . '/parts/scripts.php'; ?>
+<?php include __DIR__ . '/parts/html-footer.php'; ?>
